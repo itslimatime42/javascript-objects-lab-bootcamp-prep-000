d24c9e47f8169ccf47be438d92c4a57a8d37203c
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 06:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(obj, key) {
-  newObj = 
+  newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj
 }
